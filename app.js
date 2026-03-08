3a3d357728815a86fe13009e47a480dd0e713535
MistyHaven <mistyhaven150@gmail.com>
2026-03-07 22:35:05 -0500

    more updates to app.js code

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ var surveyController = require('./surveyController');
 
 var app = express();
 
+var urlEncodedSurvey = surveyController.urlencoded({extended : false});
+
+
 // set up template engine
 app.set('view engine', 'ejs');
+
+
+app.get('/index', function(req,res){
+    res.render('index', {qs: req.query});
+
+});
+
+app.post('/index', urlEncodedSurvey, function(req,res){
+    console.log(req.body);
+    res.render('index', {qs: req.query});
+});
+
 
 // static file serving
 app.use(express.static('./public'));
