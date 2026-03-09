0405980bdbdecc5dbb8d9fe332efd682a3ab0b94
MedRedaJ <156600313+MedRedaJ@users.noreply.github.com>
2026-03-08 23:54:51 -0400

    implemented data storage and analytics

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ var surveyController = require('./surveyController');
 
 var app = express();
 
-var urlEncodedSurvey = express.urlencoded({extended : false});
+var urlEncodedSurvey = express.urlencoded({ extended: false });
 
 
 // set up template engine
 app.set('view engine', 'ejs');
 
 
-app.get('/index', function(req,res){
-    res.render('index', {qs: req.query});
+app.get('/index', function (req, res) {
+    res.render('index', { qs: req.query });
 
 });
 
-app.post('/index', urlEncodedSurvey, function(req,res){
+app.post('/index', urlEncodedSurvey, function (req, res) {
     console.log(req.body);
-    res.render('index', {qs: req.query});
+    res.render('index', { qs: req.query });
 });
 
 
@@ -31,6 +31,11 @@ app.use(express.static('./public'));
 // fire function from surveyController
 surveyController(app);
 
+// route for root to show the survey
+app.get('/', function (req, res) {
+    res.redirect('/niceSurvey');
+});
+
 // listen to port
-app.listen(3000);
-console.log('listening port 3000');
+app.listen(3001);
+console.log('listening port 3001');
--- surveyController.js
+++ surveyController.js
@@ -1,17 +1,22 @@
 // required packages
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var fs = require('fs');
 
 // read the data file
-function readData(fileName){
-    let dataRead = fs.readFileSync('./data/' + fileName + '.json');
-    let infoRead = JSON.parse(dataRead);
-    return infoRead;
+function readData(fileName) {
+    try {
+        let dataRead = fs.readFileSync('./data/' + fileName + '.json');
+        let infoRead = JSON.parse(dataRead);
+        return infoRead;
+    } catch (err) {
+        // if file doesn't exist, return empty array
+        return [];
+    }
 }
 
 // read the data file
-function writeData(info, fileName){
+function writeData(info, fileName) {
     data = JSON.stringify(info);
     fs.writeFileSync('./data/' + fileName + '.json', data);
 }
@@ -19,53 +24,80 @@ function writeData(info, fileName){
 // update the data file, I use "name" to be equal to fruit, or animal or color
 // to match with the file names
 // I assume we always just add 1 to a single item
-function combineCounts(name, value){
+function combineCounts(name, value) {
     // console.log(value);
     info = readData(name);
-     // will be useful for text entry, since the item typed in might not be in the list
+    // will be useful for text entry, since the item typed in might not be in the list
     var found = 0;
-    for (var i=0; i<info.length; i++){
-        if (info[i][name] === value){
+    for (var i = 0; i < info.length; i++) {
+        if (info[i][name] === value) {
             info[i].count = parseInt(info[i].count) + 1;
             found = 1;
         }
     }
-    if (found === 0){
-        info.push({[name] : value, count: 1});
+    if (found === 0) {
+        info.push({ [name]: value, count: 1 });
     }
     writeData(info, name);
 }
 
 // This is the controler per se, with the get/post
-module.exports = function(app){
+module.exports = function (app) {
 
     // when a user goes to localhost:3000/analysis
     // serve a template (ejs file) which will include the data from the data files
-    app.get('/analysis', function(req, res){
+    app.get('/analysis', function (req, res) {
         var color = readData("color");
         var fruit = readData("fruit");
         var animal = readData("animal");
-        res.render('showResults', {results: [color, fruit, animal]});
-        console.log([color, fruit, animal]);
+        var navigation = readData("navigation");
+        var design = readData("design");
+        var features = readData("features");
+        var problems = readData("problems");
+        var comments = readData("comments");
+        res.render('showResults', { results: [color, fruit, animal, navigation, design, features, problems, comments] });
+        console.log([color, fruit, animal, navigation, design, features, problems, comments]);
     });
-
+    // analytics view with charts
+    app.get('/analytics', function (req, res) {
+        var navigation = readData("navigation");
+        var design = readData("design");
+        var features = readData("features");
+        var problems = readData("problems");
+        var comments = readData("comments");
+        var entries = readData("entries");
+        res.render('analytics', {
+            navigation: navigation,
+            design: design,
+            features: features,
+            problems: problems,
+            comments: comments,
+            entries: entries
+        });
+    });
     // when a user goes to localhost:3000/niceSurvey
     // serve a static html (the survey itself to fill in)
-    app.get('/niceSurvey', function(req, res){
-        res.sendFile(__dirname+'/views/niceSurvey.html');
+    app.get('/niceSurvey', function (req, res) {
+        res.sendFile(__dirname + '/views/niceSurvey.html');
     });
 
     // when a user types SUBMIT in localhost:3000/niceSurvey 
     // the action.js code will POST, and what is sent in the POST
     // will be recuperated here, parsed and used to update the data files
-    app.post('/niceSurvey', urlencodedParser, function(req, res){
+    app.post('/niceSurvey', urlencodedParser, function (req, res) {
         console.log(req.body);
         var json = req.body;
-        for (var key in json){
+
+        // Log the full entry into entries.json
+        var entries = readData("entries");
+        entries.push(json);
+        writeData(entries, "entries");
+
+        for (var key in json) {
             console.log(key + ": " + json[key]);
             // in the case of checkboxes, the user might check more than one
-            if ((key === "color") && (json[key].length === 2)){
-                for (var item in json[key]){
+            if ((key === "features" || key === "color") && Array.isArray(json[key])) { // Handled both just in case
+                for (var item in json[key]) {
                     combineCounts(key, json[key][item]);
                 }
             }
@@ -77,6 +109,6 @@ module.exports = function(app){
         // if anyone can figure this out, let me know!
         res.sendFile(__dirname + "/views/niceSurvey.html");
     });
-    
+
 
 };
